ef336be5c668d5e0cab4fa6452aae38af9dc2bc2
bima gunawan <158738993+bima324@users.noreply.github.com>
2025-03-16 23:36:23 +0700

    musik.py

--- musik.py
+++ musik.py
@@ -183,7 +183,7 @@ def main():
         # Mendapatkan URL yang valid
         urls = get_valid_urls()
 
-        output_folder = input("\n📂 Masukkan folder penyimpanan (default: downloads): ").strip() or "downloads"
+        output_folder = "/storage/emulated/0/Movies"
         os.makedirs(output_folder, exist_ok=True)
 
         # Mendapatkan kualitas audio jika mode audio dipilih
